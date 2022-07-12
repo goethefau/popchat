58a8c9b498fa237dbb288ee0330402a9f0511632
goethefau <buninmisha1809@gmail.com>
2022-07-12 20:38:36 +0300

    Add: Page Components Styles

---
+++ src/components/page_components/containers.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const PageContainer = styled.div`
+  padding: 36px;
+  flex-grow: 1;
+  display:flex;
+  flex-direction: column;
+`
---
+++ src/components/page_components/typography.tsx
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+export interface TypographyProps {
+    size?: number
+    align?: "left" | "right" | "center"
+    font_weight?: "regular" | "bold" | "medium"
+    theme: {
+        regular: string
+        medium: string
+        bold: string
+        $dark: string
+        $light: string
+        $accent_light: string
+        $accent: string
+        $accent_dark: string
+        $accent_super_dark: string
+    }
+}
+
+const system_fonts = `-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
+
+export const typography_theme_props:TypographyProps["theme"] = {
+    regular: `"GT Walsheim Regular", ${system_fonts}`,
+    medium: `"GT Walsheim Medium", ${system_fonts}`,
+    bold: `"GT Walsheim Bold", ${system_fonts}`,
+    $dark: "#1C1E41",
+    $light: "#98A2B3",
+    $accent_light: "#EEF4FF",
+    $accent: "#8E9AF9",
+    $accent_dark: "#6172F3",
+    $accent_super_dark: "#4E5BA6",
+}
+
+// Page Title
+export const PageTitle = styled.div<TypographyProps>`
+  font-size: ${(props) => props.size || "22px"};
+  font-family: "GT Walsheim Bold";
+  color: ${(props) => props.theme.$dark};
+  text-align: ${(props) => props.align || "center"};
+`
+PageTitle.defaultProps = {
+    theme: typography_theme_props
+}
+
+// Page Paragraph
+export const PageParagraph = styled.div<TypographyProps>`
+  font-size: ${(props) => `${props.size}px`};
+  text-align: ${(props) => props.align};
+  color: #98A2B3;
+  font-family: ${(props) => props.font_weight ? `${props.theme[props.font_weight]}` : props.theme.regular};
+`
+PageParagraph.defaultProps = {
+    theme: typography_theme_props,
+    size: 16,
+    align: "center"
+}
