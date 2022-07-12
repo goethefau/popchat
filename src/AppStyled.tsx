6fbc2a41d9663579742de1f480be239caca97357
goethefau <buninmisha1809@gmail.com>
2022-07-12 20:37:13 +0300

    Add: App Styles

---
+++ src/AppStyled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AppStyled = styled.div`
+  position: absolute;
+  top: 0;
+  left:0;
+  width:100%;
+  height:100%;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`
