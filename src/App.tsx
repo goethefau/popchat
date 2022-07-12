6ef49661bd39e583474bd186b90a3fbabef28f7b
goethefau <buninmisha1809@gmail.com>
2022-07-12 20:39:58 +0300

    Add: App routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-const App:React.FC = () => {
-  return (
-    <div className="App">
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Homepage from "./components/homepage/homepage";
+import {AppStyled} from "./AppStyled";
+import ChatPage from "./components/chat/chat-page";
 
-    </div>
-  );
+const App: React.FC = () => {
+
+    return (
+        <AppStyled className="app">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage/>}/>
+                    <Route path="/chat">
+                        <Route path=":chat_id" element={<ChatPage/>}/>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AppStyled>
+    );
 }
 
-export default App;
+export default App
